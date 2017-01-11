eafc5630f77994c045858ee3b1ad381d85df19a9
nalssing <nalssing@8percent.kr>
2017-01-11 17:24:31 +0900

    ADD: github-flow-sugar formula

---
+++ Formula/github-flow-sugar.rb
@@ -0,0 +1,14 @@
+class GithubFlowSugar < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/8percent/github-flow-sugar/archive/v0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "593b3e9ddf880231252c6e3e9304d9bc47577335a362056619d68f0e92bf3fd5"
+
+  COMMANDS = %w(git-feature git-pr git-release)
+
+  def install
+    system *%W(install -d -m 755 #{bin})
+    system *%W(install -m 755), *COMMANDS, bin.to_s
+  end
+end
