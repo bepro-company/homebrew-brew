6ecab865ca5a89d296da4e1d1a251595f3d5d7b0
nalssing <nalssing@8percent.kr>
2017-01-12 10:36:48 +0900

    FIX: version up

--- Formula/github-flow-sugar.rb
+++ Formula/github-flow-sugar.rb
@@ -2,7 +2,7 @@ class GithubFlowSugar < Formula
   desc ""
   homepage ""
   url "https://github.com/8percent/github-flow-sugar/archive/v0.0.3.tar.gz"
-  version "0.0.2"
+  version "0.0.3"
   sha256 "ea3324e8ea8cec9b4959357beec0c0f44e3bdab788003b64163146b12ebd4c4b"
 
   COMMANDS = %w(git-feature git-pr git-release)
