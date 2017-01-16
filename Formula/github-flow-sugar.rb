1734c3bbb755e4a9882db99532ede58f0403af3a
nalssing <nalssing@8percent.kr>
2017-01-16 10:40:43 +0900

    version 0.0.4

--- Formula/github-flow-sugar.rb
+++ Formula/github-flow-sugar.rb
@@ -1,11 +1,11 @@
 class GithubFlowSugar < Formula
   desc ""
   homepage ""
-  url "https://github.com/8percent/github-flow-sugar/archive/v0.0.3.tar.gz"
-  version "0.0.3"
-  sha256 "ea3324e8ea8cec9b4959357beec0c0f44e3bdab788003b64163146b12ebd4c4b"
+  url "https://github.com/8percent/github-flow-sugar/archive/v0.0.4.tar.gz"
+  version "0.0.4"
+  sha256 "b31e27bebdc0b9684e1d123fede06fd79e9000dcc6c4ba1808bb668fc64ef869"
 
-  COMMANDS = %w(git-feature git-pr git-release)
+  COMMANDS = %w(git-feature git-pr git-release git-mr git-pub)
 
   def install
     system *%W(install -d -m 755 #{bin})
